80a86e54484faacffd2b3a232cec57e04304fe9b
irrelevantRyan <65325330+Rhyzhang@users.noreply.github.com>
2022-09-16 07:48:07 -0400

    say can

--- 01_🤖_Main.py
+++ 01_🤖_Main.py
@@ -12,16 +12,30 @@ st.set_page_config(
 
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
+from streamlit_ace import st_ace
+
+# Spawn a new Ace editor
+content = st_ace(value="/*\nProgram Description: This program is a RobotC program\n\nRobot Description: The robot has 4 motors with 4 wheels.\n*/\n\nint main()\n{\n\n    ", language="c")
+
+
+# Display editor's content as you type
+content
+
+
 # Instruction input
 if 'instructions' not in st.session_state:
     st.session_state['instructions'] = ['Stop']
 
 new_instruction = st.text_input("Add Instruction")
-if st.button("Add instruction"):
+col1, col2 = st.columns(2)
+if col1.button("Add instruction"):
     st.session_state['instructions'].insert(-1, new_instruction)
+elif col2.button("Clear Instructions"):
+    st.session_state['instructions'] = ['Stop']
+    st.experimental_rerun()
 
 instructions_incode = ""
-instructions_prompt = "Complete the code below to execute the instructions:\n"
+instructions_prompt = "Edit and complete the code below to execute the instructions:\n"
 for index, instruction in enumerate(st.session_state['instructions']):
     st.caption(f"{index + 1}. {instruction}")
     instructions_prompt += f"    {index + 1}. {instruction}\n"
