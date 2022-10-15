4ea6f24e4b0bf97b1663e5309a98cec7dff02d14
irrelevantRyan <65325330+Rhyzhang@users.noreply.github.com>
2022-10-15 17:27:26 -0400

    Cleaning up code

--- model.py
+++
@@ -1,9 +0,0 @@
-from transformers import pipeline
-
-generator = pipeline("text-generation", model="distilgpt2")
-output = generator(
-    "Once Upon a time, there was a little boy who had a dream. A dream many did not believe was possible. Yet, he tried",
-    max_length=100,
-)
-
-print(output)
