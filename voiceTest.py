c2346c5361282da99fa83a602c748b3df010a2fb
daniel-halpern <dhalpern4040@gmail.com>
2022-10-20 22:44:21 -0400

    Added a start and stop keyword

--- voiceTest.py
+++ voiceTest.py
@@ -3,11 +3,13 @@
 import speech_recognition as sr
 
 r = sr.Recognizer()
+steps = []
+boolean = False
+bool2 = True
 
-while True:
+while bool2 == True:
     try:
         with sr.Microphone() as mic:
-
             r.adjust_for_ambient_noise(mic, duration=.2)
             audio = r.listen(mic)
 
@@ -16,8 +18,23 @@ while True:
 
             print(f"Recognized {text}")
 
-    #except sr.UnknownValueError():
+            if "hello computer" in text and boolean == False:
+                steps.append(text.replace("hello computer", ''))      
+                boolean = True
+                print(steps)
+            elif "goodbye computer" in text and boolean == True:
+                steps.append(text.replace("goodbye computer", ''))
+                print(steps)
+                bool2 = False
+            elif boolean == True:
+                steps.append(text)
+                print(steps)
+
     except:
 
         r = sr.Recognizer()
         continue
+steps = list(filter(None, steps))
+print(f"You said: {steps}")
+
+
